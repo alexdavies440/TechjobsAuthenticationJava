68018bfef5132a4c03e978a073db0a0649b3f906
alexdavies440 <alexdavies440@gmail.com>
2024-11-17 17:37:13 -0600

    Finished User class, created UserRepository

---
+++ src/main/java/org/launchcode/techjobsauth/models/User.java
@@ -0,0 +1,35 @@
+package org.launchcode.techjobsauth.models;
+
+import com.mysql.cj.conf.StringProperty;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Entity
+public class User extends AbstractEntity {
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String pwHash;
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public User() {}
+
+    // Encoder here stores hashed password instead of password itself for security
+    public User(String username, String password) {
+        this.username = username;
+        this.pwHash = encoder.encode(password);
+    }
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
+    }
+
+    public @NotNull String getUsername() {
+        return username;
+    }
+
+}
