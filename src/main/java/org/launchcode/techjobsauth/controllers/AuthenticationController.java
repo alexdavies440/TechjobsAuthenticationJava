2de272a8ccc82dfa0cf284865c14f9ae22e56b2e
alexdavies440 <alexdavies440@gmail.com>
2024-11-17 18:27:27 -0600

    Register template

--- src/main/java/org/launchcode/techjobsauth/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/techjobsauth/controllers/AuthenticationController.java
@@ -1,10 +1,17 @@
 package org.launchcode.techjobsauth.controllers;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.launchcode.techjobsauth.models.User;
 import org.launchcode.techjobsauth.models.data.UserRepository;
+import org.launchcode.techjobsauth.models.dto.RegisterFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
 import java.util.Optional;
@@ -36,6 +43,14 @@ public class AuthenticationController {
     // 3.
     private static void SetUserInSession(HttpSession session, User user) {
         session.setAttribute(userSessionKey, user.getId());
+    }
+
+    // 4.1
+    @GetMapping("/register")
+    public String displayRegisterForm(Model model) {
+        model.addAttribute("title", "Register");
+        model.addAttribute(new RegisterFormDTO());
+        return "register";
     }
 
 
